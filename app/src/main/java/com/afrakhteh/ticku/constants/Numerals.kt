0089c8a7db2ad6135098e53d42e38eb28b59189a
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2022-02-08 12:54:48 +0330

    Create DataBase

---
+++ app/src/main/java/com/afrakhteh/ticku/constants/Numerals.kt
@@ -0,0 +1,7 @@
+package com.afrakhteh.ticku.constants
+
+class Numerals {
+    companion object {
+        const val DATABASE_VERSION = 1
+    }
+}
---
+++ app/src/main/java/com/afrakhteh/ticku/model/data/database/TaskDataBase.kt
@@ -0,0 +1,15 @@
+package com.afrakhteh.ticku.model.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.afrakhteh.ticku.constants.Numerals
+import com.afrakhteh.ticku.model.entities.Tasks
+
+@Database(
+    entities = [Tasks::class],
+    version = Numerals.DATABASE_VERSION,
+    exportSchema = false
+)
+abstract class TaskDataBase : RoomDatabase(){
+    abstract fun dao(): TaskDao
+}
