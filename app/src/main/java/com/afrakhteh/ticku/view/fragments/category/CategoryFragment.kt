d6ae8fc679c6884ba0421527ddf417ce48021b3a
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2022-02-09 16:56:21 +0330

    Delete unnecessary fragments and add new icon

--- app/src/main/java/com/afrakhteh/ticku/view/fragments/category/CategoryFragment.kt
+++ app/src/main/java/com/afrakhteh/ticku/view/fragments/category/CategoryFragment.kt
@@ -1,4 +1,4 @@
-package com.afrakhteh.ticku.view.fragments.shopping
+package com.afrakhteh.ticku.view.fragments.category
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,13 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.afrakhteh.ticku.R
 
-class ShoppingFragment : Fragment() {
+class CategoryFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping, container, false)
+        return inflater.inflate(R.layout.fragment_category, container, false)
     }
+
 }
