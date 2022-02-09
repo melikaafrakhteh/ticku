c6f1ba23455401d53c5008e04425a74dfdaf21b6
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2022-02-09 18:32:04 +0330

    Add ViewBinding

--- app/src/main/java/com/afrakhteh/ticku/view/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/afrakhteh/ticku/view/fragments/home/HomeFragment.kt
@@ -6,19 +6,63 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.afrakhteh.ticku.R
+import com.afrakhteh.ticku.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var homeBinding: FragmentHomeBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val binding = FragmentHomeBinding.inflate(inflater, container, false)
+        homeBinding = binding
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        itemClicks()
+    }
 
+    private fun itemClicks() {
+        with(requireNotNull(homeBinding)) {
+            homeSearchEditText.setOnClickListener(::goToSearchPage)
+            homeSchoolLinear.setOnClickListener(::goToSchoolCategoryPage)
+            homeHolidayLinear.setOnClickListener(::goToHolidayCategoryPage)
+            homeBusinessLinear.setOnClickListener(::goToBusinessCategoryPage)
+            homeShoppingLinear.setOnClickListener(::goToShoppingCategoryPage)
+            homeFab.setOnClickListener(::addNewTask)
+        }
+    }
+
+    private fun addNewTask(view: View?) {
+
+    }
+
+    private fun goToShoppingCategoryPage(view: View?) {
+
+    }
+
+    private fun goToBusinessCategoryPage(view: View?) {
+
+    }
+
+    private fun goToHolidayCategoryPage(view: View?) {
+
+    }
+
+    private fun goToSchoolCategoryPage(view: View?) {
+
+    }
+
+    private fun goToSearchPage(view: View?) {
+
+    }
+
+    override fun onDestroy() {
+        homeBinding = null
+        super.onDestroy()
     }
 }
