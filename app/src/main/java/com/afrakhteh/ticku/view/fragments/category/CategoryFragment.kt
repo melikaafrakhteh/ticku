5656ec4ac4baba8961f52d3d8e4e1dc6df776f35
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2022-02-23 15:46:14 +0330

    Add viewbinding to category fragment

--- app/src/main/java/com/afrakhteh/ticku/view/fragments/category/CategoryFragment.kt
+++ app/src/main/java/com/afrakhteh/ticku/view/fragments/category/CategoryFragment.kt
@@ -6,15 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.afrakhteh.ticku.R
+import com.afrakhteh.ticku.databinding.FragmentCategoryBinding
 
 class CategoryFragment : Fragment() {
+
+    private var categoryBinding: FragmentCategoryBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        val binding = FragmentCategoryBinding.inflate(layoutInflater, container, false)
+        categoryBinding = binding
+        return binding.root
     }
 
+    override fun onDestroy() {
+        categoryBinding = null
+        super.onDestroy()
+    }
 }
