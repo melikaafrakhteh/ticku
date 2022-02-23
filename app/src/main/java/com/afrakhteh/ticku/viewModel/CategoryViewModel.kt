e9f187349c2f89d42ca041b6db36bee363c94425
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2022-02-23 16:32:25 +0330

    Implement delete and getAll functions in category viewModel

--- app/src/main/java/com/afrakhteh/ticku/viewModel/CategoryViewModel.kt
+++ app/src/main/java/com/afrakhteh/ticku/viewModel/CategoryViewModel.kt
@@ -1,11 +1,30 @@
 package com.afrakhteh.ticku.viewModel
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.afrakhteh.ticku.di.qualifier.IoDispatcher
+import com.afrakhteh.ticku.model.entities.TaskEntity
 import com.afrakhteh.ticku.model.useCase.ListsPagesUseCases
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class CategoryViewModel @Inject constructor(
-    private val useCases: ListsPagesUseCases
-): ViewModel() {
+    private val useCases: ListsPagesUseCases,
+    @IoDispatcher private val io: CoroutineDispatcher
+) : ViewModel() {
 
+    private val pListOfData = MutableLiveData<List<TaskEntity>>()
+    val listOfData: LiveData<List<TaskEntity>> get() = pListOfData
+
+    fun getCategoryItem(type: Int) {
+        useCases.getAllTasksByTypeUseCase(type).asLiveData(io).value?.let {
+            pListOfData.postValue(it)
+        }
+    }
+
+    fun deleteAllCategoryList(type: Int) {
+        viewModelScope.launch {
+            useCases.deleteAllTaskByTypeUseCase(type)
+        }
+    }
 }
